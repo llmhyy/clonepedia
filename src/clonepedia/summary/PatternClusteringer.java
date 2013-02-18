f59041602a3e6ccd8324de56e0c5ddb3d5373c5e
Lin Yun <llmhyy@gmail.com>
2013-02-18 12:35:30 +0000

    The clone pattern (inter clone set pattern) should contain at least two clone sets.

--- src/clonepedia/summary/PatternClusteringer.java
+++ src/clonepedia/summary/PatternClusteringer.java
@@ -68,7 +68,7 @@ public class PatternClusteringer implements Serializable{
 		}
 		
 		//if(isForLocationClustering)
-		//filterThePatternClusterJustCorrespondingToOneCloneSet(clusterList);
+		filterThePatternClusterJustCorrespondingToOneCloneSet(clusterList);
 		
 		return clusterList;
 	}
