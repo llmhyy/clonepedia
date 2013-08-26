c49ec386db612a0d084e2deac62d58c51f8078b8
Lin Yun <llmhyy@gmail.com>
2013-08-26 08:43:35 +0000

    Make different the colors for completed and uncompleted class/method

--- src/clonepedia/test/syntacticresult/TestSyntacticResult.java
+++ src/clonepedia/test/syntacticresult/TestSyntacticResult.java
@@ -1,5 +1,8 @@
 package clonepedia.test.syntacticresult;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -8,15 +11,47 @@ import clonepedia.util.MinerUtil;
 
 public class TestSyntacticResult {
 
-	
+	private int a = 0;
+	private PropertyChangeSupport support = new PropertyChangeSupport(this);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String resultFile = "clusters_15.0";
+		/*String resultFile = "clusters_15.0";
 		MinerUtil mu = new MinerUtil();
 		ArrayList<SyntacticCluster> list = (ArrayList<SyntacticCluster>)mu.deserialize(resultFile, false);
-		System.out.print("");
+		System.out.print("");*/
+		
+		TestSyntacticResult tsr = new TestSyntacticResult();
+		tsr.addPoperpertyChangeListener(new PropertyChangeListener() {
+			
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				System.out.println("a changed");
+				
+			}
+		});
+		
+		tsr.setA(1);
+		
+	}
+	
+	public int getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		int oldA = this.a;
+		this.a = a;
+		support.firePropertyChange("a",	oldA, a);
+	}
+	
+	public void addPoperpertyChangeListener(PropertyChangeListener listener){
+		support.addPropertyChangeListener(listener);
+	}
+	
+	public void removePoperpertyChangeListener(PropertyChangeListener listener){
+		support.removePropertyChangeListener(listener);
 	}
 
 }
