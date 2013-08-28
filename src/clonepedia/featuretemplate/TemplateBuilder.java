b9d8698cfcb5bcc6c55a66db6db69da21ca819ad
Lin Yun <llmhyy@gmail.com>
2013-08-28 12:12:44 +0000

    Fix a bug to deal with case in which a class is non-abstracted while its containing methods are abstract Fix a bug of visualizing abstracted class

--- src/clonepedia/featuretemplate/TemplateBuilder.java
+++ src/clonepedia/featuretemplate/TemplateBuilder.java
@@ -91,8 +91,8 @@ public class TemplateBuilder {
 			clusterList = algorithm.doClustering();
 			for(NormalCluster cluster: clusterList){
 				Class abstractedClass = new Class("", null, "");
-				abstractedClass.setMerge(true);
 				if(cluster.size() > 1){
+					abstractedClass.setMerge(true);
 					for(IClusterable clusterable: cluster){
 						Class declaringClass = (Class)clusterable;
 						
@@ -122,6 +122,9 @@ public class TemplateBuilder {
 				mergeSuperType(abstractedClass);
 				//mergeInterfaces(abstractedClass);
 				mergeMethods(abstractedClass);			
+			}
+			else{
+				mergeMethods(abstractedClass);	
 			}
 		}
 	}
