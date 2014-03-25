427bca8827cba63373813792cbf49261da612497
linyun <llmhyy@gmail.com>
2014-03-25 11:15:30 +0800

    import jccd library

---
+++ src/clonepedia/actions/CloneDetectionAction.java
@@ -0,0 +1,33 @@
+package clonepedia.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+import clonepedia.util.Settings;
+
+public class CloneDetectionAction implements IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		
+	}
+
+}
