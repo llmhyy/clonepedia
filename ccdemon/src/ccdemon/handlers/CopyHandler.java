158513a71e019b72777915f5382314a5d41d871f
xxx_di <xxx_di@qq.com>
2014-11-05 19:40:05 +0800

    Issue #1 : Construct a SelectedCodeRange Object from copied code

--- ccdemon/src/ccdemon/handlers/CopyHandler.java
+++ ccdemon/src/ccdemon/handlers/CopyHandler.java
@@ -3,8 +3,18 @@ package ccdemon.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 
+import ccdemon.model.SelectedCodeRange;
 import ccdemon.util.CCDemonUtil;
+import ccdemon.util.SharedData;
 
 public class CopyHandler extends AbstractHandler{
 
@@ -15,7 +25,14 @@ public class CopyHandler extends AbstractHandler{
 		/**
 		 * record the location of copied code
 		 */
+		AbstractTextEditor activeEditor = (AbstractTextEditor) HandlerUtil.getActiveEditor(event);
+		ISourceViewer sourceViewer = (ISourceViewer) activeEditor.getAdapter(ITextOperationTarget.class);		
+		IEditorInput input = activeEditor.getEditorInput(); 
+		IFile file = ((FileEditorInput) input).getFile(); 		
+		Point point = sourceViewer.getSelectedRange();
 		
+		SelectedCodeRange range = new SelectedCodeRange(file.getRawLocation().toOSString(), point.x, point.x + point.y);
+		SharedData.range = range;
 		
 		System.out.println("copy");
 		return null;
