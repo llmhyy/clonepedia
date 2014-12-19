042186fd38c7fe2aa3f9833c2bb5ae84bbbcf715
linyun <llmhyy@gmail.com>
2014-12-19 20:54:19 +0800

    Adjust the naming rule matching threshold, in order to make the instance could be matched to the naming rule (pattern) as much as possible

--- ccdemon/src/ccdemon/model/rule/NamingRule.java
+++ ccdemon/src/ccdemon/model/rule/NamingRule.java
@@ -24,7 +24,7 @@ public class NamingRule {
 	 */
 	public void applyRule(String candidateString, ConfigurationPoint currentPoint){
 		refreshNamingModel(candidateString, currentPoint);
-		
+		 
 		if(!currentPoint.getCopiedTokenSeq().isSingleToken()){
 			return;
 		}
