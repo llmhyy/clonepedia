2acc2f02ebe99fea91a336762182a796551b1cfe
xxx_di <xxx_di@qq.com>
2014-12-23 20:03:26 +0800

    set the test package to an older version

--- ccdemon/test/test/TestSub.java
+++ ccdemon/test/test/TestSub.java
@@ -8,15 +8,13 @@ import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 
-import types.Parent;
-
 public class TestSub {
 	public static void main(String[] args){
 		ConfigurationBuilder cb = new ConfigurationBuilder().setScanners(new SubTypesScanner());
-		cb.addUrls(ClasspathHelper.forClass(Parent.class));
+		cb.addUrls(ClasspathHelper.forClass(IDocument.class));
 	    Reflections reflections = new Reflections(cb);
 	
-		Set<Class<? extends Parent>> subset = reflections.getSubTypesOf(Parent.class);
+		Set<Class<? extends IDocument>> subset = reflections.getSubTypesOf(IDocument.class);
 		System.out.println(subset.toString());
 	}
 }
