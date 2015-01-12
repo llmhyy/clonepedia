65e96c9219822ad74929a0a5ab1332469770f048
xxx_di <xxx_di@qq.com>
2015-01-12 13:21:07 +0800

    1) consider more context when computing the optimal common subsequence. 2) adjust the heuristic of "else" problem. 3) the largest AST node to split should be a statement. 4) threshold in matching strategy could be 0 now.

--- mcidiff/src/mcidiff/util/TokenSimilarityComparator.java
+++ mcidiff/src/mcidiff/util/TokenSimilarityComparator.java
@@ -15,19 +15,31 @@ public class TokenSimilarityComparator{
 		ASTNode node2 = token2.getNode();
 		
 		if(node1 != null && node2 != null){
-			Character[] str1 = transferChar(node1.toString());
-			Character[] str2 = transferChar(node2.toString());
 			
-			double length = DiffUtil.buildLeveshteinTable(str1, str2, new DefaultComparator())[str1.length][str2.length];
 			
-			double textualSim = 2*length/(str1.length+str2.length);
+			double textualSim = computeNodeSim(token1.getNode(), token2.getNode());
+			double contextualSim = computeNodeSim(node1.getParent(), node2.getParent());
 			double positionSim = 1-Math.abs(token1.getRelativePositionRatio() - token2.getRelativePositionRatio());
 			
-			return 0.5*textualSim + 0.5*positionSim;
+			//double avgWeight = (1.0)/3;
+			return 0.8*contextualSim + 0.1*textualSim + 0.1*positionSim;
 		}
 		
 		return 0;
-	};
+	};	
+	
+	private double computeNodeSim(ASTNode node1, ASTNode node2){
+		if(node1 == null || node2 == null){
+			return 0;
+		}
+		
+		Character[] str1 = transferChar(node1.toString());
+		Character[] str2 = transferChar(node2.toString());
+		
+		double length = DiffUtil.buildLeveshteinTable(str1, str2, new DefaultComparator())[str1.length][str2.length];
+		
+		return 2*length/(str1.length+str2.length);
+	}
 	
 	private Character[] transferChar(String string){
 		char[] chars = string.toCharArray();
