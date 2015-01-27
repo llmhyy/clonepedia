6fa5ae7337a3aaf1215fbad3a0a1ccef0620ba31
linyun <llmhyy@gmail.com>
2015-01-27 16:20:44 +0800

    change the screen ratio of different views in clone diff perspective

--- clonepedia/src/clonepedia/perspective/CloneDiffPerspective.java
+++ clonepedia/src/clonepedia/perspective/CloneDiffPerspective.java
@@ -8,17 +8,17 @@ public class CloneDiffPerspective implements IPerspectiveFactory {
 	public static String CLONE_REPORT_VIEW = "Clonepedia.cloneReportView";
 	public static String CLONE_DIFF_VIEW = "Clonepedia.CloneDiffView";
 	public static String DIFF_PROPERTY_VIEW = "Clonepedia.DiffPropertyView";
-	public static String TASK_LIST_VIEW = "org.eclipse.ui.views.TaskList";
+	public static String TASK_LIST_VIEW = "org.eclipse.mylyn.tasks.ui.views.tasks";
 	
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
-		layout.addView(CLONE_REPORT_VIEW, IPageLayout.LEFT, 0.25f, editorArea);
+		layout.addView(CLONE_REPORT_VIEW, IPageLayout.LEFT, 0.15f, editorArea);
 		
 		layout.addView(CLONE_DIFF_VIEW, IPageLayout.BOTTOM, 0.55f, editorArea);
 		
-		layout.addView(DIFF_PROPERTY_VIEW, IPageLayout.RIGHT, 0.3f, editorArea);
-		layout.addView(TASK_LIST_VIEW, IPageLayout.RIGHT, 0.3f, editorArea);
+		//layout.addView(DIFF_PROPERTY_VIEW, IPageLayout.RIGHT, 0.3f, editorArea);
+		layout.addView(TASK_LIST_VIEW, IPageLayout.RIGHT, 0.7f, editorArea);
 	}
 
 }
