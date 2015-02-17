7e962d94fcf6f9abac921399709fa53cfecde90c
xxx_di <xxx_di@qq.com>
2015-02-17 12:04:06 +0800

    jassperreports is too big, only analyze and export type3 first

--- ccdemon/evaluation/ccdemon/evaluation/handler/AnalyzeStatisticsHandler.java
+++ ccdemon/evaluation/ccdemon/evaluation/handler/AnalyzeStatisticsHandler.java
@@ -2,7 +2,11 @@ package ccdemon.evaluation.handler;
 
 import java.util.ArrayList;
 
+import mcidiff.main.SeqMCIDiff;
+import mcidiff.model.CloneInstance;
 import mcidiff.model.CloneSet;
+import mcidiff.model.SeqMultiset;
+import mcidiff.model.TokenSeq;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -11,6 +15,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IJavaProject;
 
 import ccdemon.evaluation.main.CloneRecoverer;
 import ccdemon.evaluation.main.CloneRecoverer.CollectedData;
@@ -54,6 +59,13 @@ public class AnalyzeStatisticsHandler extends AbstractHandler {
 			CloneSet set = CCDemonUtil.adaptMCIDiffModel(clonepediaSet);
 			if(set.getInstances().size() < 3){
 				continue;
+			}
+			try {
+				if(!isTypeIII(set)){
+					continue;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			ArrayList<CollectedData> datas = recoverer.getTrials(set);
 			
@@ -106,7 +118,71 @@ public class AnalyzeStatisticsHandler extends AbstractHandler {
 			
 		}
 		
-		exporter.end("JHotDraw");
+		exporter.end("jasperreports-type3");
 	}
 
+	private boolean isTypeIII(CloneSet set) throws Exception{
+		SeqMCIDiff diff = new SeqMCIDiff();
+		IJavaProject proj = CCDemonUtil.retrieveWorkingJavaProject();
+		ArrayList<SeqMultiset> diffList;
+		diffList = diff.diff(set, proj);
+		
+		ArrayList<SeqMultiset> matchableDiffs = findMatchableDiff(diffList, set.getInstances().get(0));
+		
+		boolean isTypeIII = false;
+		for(int i = 0; i < matchableDiffs.size() && !isTypeIII; i++){
+			SeqMultiset seqMulti = matchableDiffs.get(i);
+			for(TokenSeq seq : seqMulti.getSequences()){
+				if(seq.isEpisolonTokenSeq()){
+					isTypeIII = true;
+					break;
+				}
+			}
+		}
+		
+		return isTypeIII;
+	}
+	
+
+	
+	/**
+	 * Given a target instance, the diffs can be divided into two categories: 
+	 * Type I: The token sequence for target instance is different from the token sequences for other instances, 
+	 * in this case, those token sequences in other instances are exactly the same.
+	 * Type II (i.e., matchable diff): The token sequences for other instances are different with each other.
+	 * 
+	 * By differentiating these differences, I am able to identify the correct candidate for each configuration
+	 * point.
+	 * 
+	 * @param diffList
+	 * @return
+	 */
+	private ArrayList<SeqMultiset> findMatchableDiff(ArrayList<SeqMultiset> diffList, CloneInstance targetInstance){
+		ArrayList<SeqMultiset> typeTwoDiffList = new ArrayList<>();
+		
+		for(SeqMultiset diff : diffList){
+			//TokenSeq targetSeq = diff.findTokenSeqByCloneInstance(targetInstance);
+			int difference = 0;
+			for(int i = 0; i < diff.getSequences().size(); i++){
+				if(!diff.getSequences().get(i).getCloneInstance().equals(targetInstance)){
+					for(int j = i + 1; j < diff.getSequences().size(); j++){
+						if(!diff.getSequences().get(j).getCloneInstance().equals(targetInstance)){
+							if(!diff.getSequences().get(i).equals(diff.getSequences().get(j))){
+								difference++;
+							}
+						}
+					}
+				}
+			}
+			if(difference != 0){
+				SeqMultiset newDiff = new SeqMultiset();
+				for(TokenSeq seq : diff.getSequences()){
+					newDiff.addTokenSeq(seq);
+				}
+				typeTwoDiffList.add(newDiff);
+			}
+		}
+		
+		return typeTwoDiffList;
+	}
 }
